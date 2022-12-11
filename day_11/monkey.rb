5eac4fad379d1a25e0d58e295ff65514c07eb7b7
Alexander Samolov <alexsamolov@gmail.com>
2022-12-11 15:02:11 +0100

    day 11 part 2

--- day_11/monkey.rb
+++ day_11/monkey.rb
@@ -6,11 +6,12 @@ file = ARGV[0]
 puts("Reading ", file)
 
 class Monkey
-    def initialize(n, items, opProc, divTest, monkeyIfTrue, monkeyIfFalse)
+    def initialize(n, items, opProc, divider, monkeyIfTrue, monkeyIfFalse)
         @n = n
         @items = items.dup
         @opProc = opProc
-        @nextMonkeyProc = -> (item) { item % divTest == 0 ? monkeyIfTrue : monkeyIfFalse}
+        @nextMonkeyProc = -> (item) { item % divider == 0 ? monkeyIfTrue : monkeyIfFalse}
+        @divider = divider
         @inspected = 0
     end
 
@@ -26,20 +27,24 @@ class Monkey
         @inspected
     end
 
+    def divider
+        @divider
+    end
+
     # returns list of pairs [monkey, item]
-    def round()
-        puts "Monkey #{@n}:"
+    def round(reduce_stress = true)
+        # puts "Monkey #{@n}:"
         result = []
         while @items.size > 0 do
             item = @items.shift
-            puts "  Monkey inspects an item with a worry level of #{item}."
+            # puts "  Monkey inspects an item with a worry level of #{item}."
             @inspected += 1
             item = @opProc.call(item)
-            puts "    Worry level increased to #{item}."
-            item = item / 3
-            puts "    Monkey gets bored with item. Worry level is divided by 3 to #{item}."            
+            # puts "    Worry level increased to #{item}."
+            item = reduce_stress ? item / 3 : item
+            # puts "    Monkey gets bored with item. Worry level is divided by 3 to #{item}."            
             monkey = @nextMonkeyProc.call(item)
-            puts "    Item with worry level #{item} is thrown to monkey #{monkey}."
+            # puts "    Item with worry level #{item} is thrown to monkey #{monkey}."
             result.push([monkey, item])
         end
         return result
@@ -49,7 +54,7 @@ end
 def parse(n, lines)
     items = []
     opProc = nil
-    divTest = 0
+    divider = 0
     monkeyIfTrue = -1
     monkeyIfFalse = -1
     p lines
@@ -68,7 +73,7 @@ def parse(n, lines)
             arg = $1.to_i
             opProc = ->(old) { old * arg }
         when /Test: divisible by (\d+)/
-            divTest = $1.to_i
+            divider = $1.to_i
         when /If true: throw to monkey (\d+)/
             monkeyIfTrue = $1.to_i
         when /If false: throw to monkey (\d+)/
@@ -77,12 +82,12 @@ def parse(n, lines)
             raise "Cannot parse line: #{line}"
         end
     end
-    return Monkey.new(n, items, opProc, divTest, monkeyIfTrue, monkeyIfFalse)
+    return Monkey.new(n, items, opProc, divider, monkeyIfTrue, monkeyIfFalse)
 end
 
 lines = IO.readlines(file, chomp: true).each
 monkeys = []
-monkey = nil
+divider = 1
 
 loop do
     line = lines.next
@@ -91,7 +96,9 @@ loop do
         n = $1.to_i
         input = []
         5.times {input.push(lines.next)}
-        monkeys.push(parse(n, input))
+        monkey = parse(n, input)
+        monkeys.push(monkey)
+        divider *= monkey.divider
     when ""
         #noop
     else
@@ -99,10 +106,10 @@ loop do
     end
 end
 
-(1..20).each do |round|
+(1..10000).each do |round|
     monkeys.each do |monkey|
-        result = monkey.round()
-        result.each {|x| monkeys[x[0]].add(x[1])}
+        result = monkey.round(false)
+        result.each {|x| monkeys[x[0]].add(x[1] % divider)}
     end
     puts "After round #{round}, the monkeys are holding items with these worry levels:"
     monkeys.each {|m| puts m}
